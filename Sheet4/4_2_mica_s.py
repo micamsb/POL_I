409ed058fef7d05ad208ff1968c0505c32af87a3
micamsb <mica.schuepbach@gmail.com>
2024-11-29 13:34:24 +0100

    Exercise 4.2

--- Sheet4/4_2_mica_s.py
+++ Sheet4/4_2_mica_s.py
@@ -2,3 +2,24 @@
 
 # Exercise 4.1
 # Postitive and negative feed-back
+
+# What is the typical fixed point structure of a negative feedback loop?
+    # A single stable fixed point is common, so the system settles at one equilibrium state.
+    # The negative feedback loop ensures that the system corrects deviation which contributes to a higher stability.
+
+# Negative feedback loops are often present in homeostatic systems. What is homeostasis? 
+# Give two examples of homeostatic systems in biology.
+    # Homeostasis is the abiloty of a biologiacal system to maintain a stable internal environment despite external fluctuations. 
+    # Examples for homeostasis systems:
+        # Blood glucose regulation (glucose levels regulated by negative feedback of insulin and glucagon)
+        # Body temperature control (temperature stability maintained by responses like sweating or shivering triggered by the hypothalamus)
+
+# Systems with positive feedback loops can exhibit bistability. What is bistability?
+    # Bistability describes a system thet has two stable fixed points. The System can exist in either of the two points depending on the conditions.
+
+# What are possible biological roles of bistability?
+    # Bistability enables irreversible transitions between states (like cell differentiation)
+    # It also allows to maintain a state when the stimulus is removed and allows decision making.
+
+# What are the constraints on cooperativity that are nessecary for bistability?
+    # For high cooperativity with bistability, the system needs a strong enough positive feedback to create two stable states but not too strong thet the system is being destabilized.
