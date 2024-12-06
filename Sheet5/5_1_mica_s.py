b43202281e29b61b8a8d7837ea9144c565c7bfca
micamsb <mica.schuepbach@gmail.com>
2024-12-06 10:42:54 +0100

    File structure sheet 5

---
+++ Sheet5/5_1_mica_s.py
@@ -0,0 +1,4 @@
+#!/usr/local/bin/python3 --no-rcs
+
+# Exercise 5.1
+# Fluorescence recovery after photobleaching
---
+++ Sheet5/5_3_mica_s.py
@@ -0,0 +1,4 @@
+#!/usr/local/bin/python3 --no-rcs
+
+# Exercise 5.3
+# Diffusion through an outer membrane pore
